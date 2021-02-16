be2439cc6d527a72da0a1922c4d43a67bab1e83e
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-16 20:08:35 -0300

    Project Created

---
+++ project-brewery/src/main/java/igor/springframework/projectbrewery/ProjectBreweryApplication.java
@@ -0,0 +1,13 @@
+package igor.springframework.projectbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBreweryApplication.class, args);
+	}
+
+}
---
+++ project-brewery/src/test/java/igor/springframework/projectbrewery/ProjectBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.springframework.projectbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

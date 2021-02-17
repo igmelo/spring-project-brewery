0782231e5494b95aced81a77308f26a210634794
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-17 11:46:44 -0300

    POST, PUT AND DELETE methods

--- project-brewery/src/main/java/igor/springframework/projectbrewery/web/controller/BeerController.java
+++ project-brewery/src/main/java/igor/springframework/projectbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package igor.springframework.projectbrewery.web.controller;
 
 import igor.springframework.projectbrewery.web.model.BeerDTO;
 import igor.springframework.projectbrewery.web.services.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,28 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDTO> getBeer(@PathVariable UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDTO beerDTO){
+        BeerDTO savedDTO = beerService.saveNewBeer(beerDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/beer/" + savedDTO.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDTO beerDTO){
+        beerService.updateBeer(beerId, beerDTO);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable UUID beerId){
+        beerService.deleteById(beerId);
     }
 }
--- project-brewery/src/main/java/igor/springframework/projectbrewery/web/services/BeerServiceImpl.java
+++ project-brewery/src/main/java/igor/springframework/projectbrewery/web/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package igor.springframework.projectbrewery.web.services;
 
 import igor.springframework.projectbrewery.web.model.BeerDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService{
 
@@ -14,5 +16,21 @@ public class BeerServiceImpl implements BeerService{
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return BeerDTO.builder().id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDTO beerDTO) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
